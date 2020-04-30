a7bf2b406d9d478b6946dcea1f486725769d6283
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-04-30 16:11:48 -0400

    added support for regular old NYT articels too

--- content.js
+++ content.js
@@ -1,4 +1,21 @@
 setTimeout(function(){
-  jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
-  jQuery("body").css('overflow', 'auto');
+  console.log('extensy hity')
+  if(jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG")){
+  	jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
+  };
+  if(jQuery("body")){
+  	jQuery("body").css('overflow', 'auto');
+  };
+  if(document.querySelector("#gateway-content")){
+  	document.querySelector("#gateway-content").remove();
+  }
+  if(document.querySelector(".css-1bd8bfl")){
+	document.querySelector(".css-1bd8bfl").remove();
+  }
+  if(document.querySelector(".css-mcm29f")){
+    document.querySelector(".css-mcm29f").style.overflow = 'auto';
+  };
 }, 800)
+
+//for nyt
+
