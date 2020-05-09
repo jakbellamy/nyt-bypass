dab27b60d3b4e0334432d49b8dca2d840d998faa
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-05-08 23:30:21 -0400

    removed jQuery and built in a function to handle reload of paywall by stopping future DOM activity

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,11 @@
+Simple paywall remover for NYT and NYT cooking!
+
 INSTALL
   1. download this repository
   2. follow this 25 second youtube video to learn how to install the extension: https://www.youtube.com/watch?v=oswjtLwCUqg
   3. that's it. now when you click on a recipe from the nyt site, the paywall will be removed.
 
 If you have any problems, lemme know :)
-
-NOTE: i was going to add support for NYT news as well, but since their site is free right now, I dunno what code i gotta write!
 
 ------- contact info -------
 email: jakbellamy@gmail.com
--- content.js
+++ content.js
@@ -1,22 +1,37 @@
-setTimeout(function(){
-	//NYT COOKING
-	if(jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG")){
-		jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
-	};
-	if(jQuery("body")){
-		jQuery("body").css('overflow', 'auto');
-	};
-	//NYT REGULAR ARTICLES
-	if(document.querySelector("#gateway-content")){
-		document.querySelector("#gateway-content").remove();
-	};
-	if(document.querySelector(".css-1bd8bfl")){
-		document.querySelector(".css-1bd8bfl").remove();
-	};
-	if(document.querySelector(".css-mcm29f")){
-		document.querySelector(".css-mcm29f").style.overflow = 'auto';
-	};
-}, 800)
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+const hijackDOM = (interval) => {
+	setTimeout(function(){
+		//NYT COOKING
+		if(document.querySelector(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG")){
+			document.querySelector(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
+		};
+		if(document.querySelector("body")){
+			document.querySelector("body").style.overflow = 'auto';
+		};
+		//NYT REGULAR ARTICLES
+		if(document.querySelector("#gateway-content")){
+			document.querySelector("#gateway-content").remove();
+		};
+		if(document.querySelector(".css-1bd8bfl")){
+			document.querySelector(".css-1bd8bfl").remove();
+		};
+		if(document.querySelector(".css-mcm29f")){
+			document.querySelector(".css-mcm29f").style.overflow = 'auto';
+		};
+	}, interval);
+	wait(interval + 500).then(() => {
+		console.clear();
+		console.log(`Paywall Removed! \b Stopping Forced DOM Loads...`);
+	})
+};
 
+const killDOM = (interval) => {
+	wait(interval).then(() => {
+		console.log('DOM Activity Successfully Killed! Happy Browsing :)');
+		window.stop();
+	});
+};
 
+hijackDOM(800);
+killDOM(2200);
