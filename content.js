a652cb36083ff69907158633a3d1cae7ba0a7c09
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-05-08 22:47:09 -0400

    prior to split

--- content.js
+++ content.js
@@ -1,21 +1,22 @@
 setTimeout(function(){
-  console.log('extensy hity')
-  if(jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG")){
-  	jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
-  };
-  if(jQuery("body")){
-  	jQuery("body").css('overflow', 'auto');
-  };
-  if(document.querySelector("#gateway-content")){
-  	document.querySelector("#gateway-content").remove();
-  }
-  if(document.querySelector(".css-1bd8bfl")){
-	document.querySelector(".css-1bd8bfl").remove();
-  }
-  if(document.querySelector(".css-mcm29f")){
-    document.querySelector(".css-mcm29f").style.overflow = 'auto';
-  };
+	//NYT COOKING
+	if(jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG")){
+		jQuery(".nytc---modal-window---windowContainer.nytc---modal-window---isShown.nytc---shared---blackBG").remove();
+	};
+	if(jQuery("body")){
+		jQuery("body").css('overflow', 'auto');
+	};
+	//NYT REGULAR ARTICLES
+	if(document.querySelector("#gateway-content")){
+		document.querySelector("#gateway-content").remove();
+	};
+	if(document.querySelector(".css-1bd8bfl")){
+		document.querySelector(".css-1bd8bfl").remove();
+	};
+	if(document.querySelector(".css-mcm29f")){
+		document.querySelector(".css-mcm29f").style.overflow = 'auto';
+	};
 }, 800)
 
-//for nyt
+
 
